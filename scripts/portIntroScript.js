35f12d4c7c4021cb189f8fbdad798c288eb78687
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-02 16:48:56 +0000

    added star design to intro section

--- scripts/portIntroScript.js
+++ scripts/portIntroScript.js
@@ -1,4 +1,21 @@
 creEl('div', ['introDiv', 'sectionMainDiv'], 'site', 0,);
 
+
+
 creEl('div', 'intLogoDiv', 'introDiv', 0, 'MF');
 creEl('div', 'intText', 'introDiv', 0 , "Hi, I'm Mark. I am a Front End Web Developer.")
+
+function createStars(amt, size){
+    for(let i = 0; i < amt; i++){
+        let xPos = Math.floor(Math.random()*90) +10;
+        let yPos = Math.floor(Math.random()*36) + 5;
+        creEl('div', ['intStar'+size, 'intStar'], 'introDiv', 0);
+        document.getElementsByClassName('intStar'+size)[i].style.left = xPos+'%';
+        document.getElementsByClassName('intStar'+size)[i].style.top = yPos+'%';
+    }
+}
+
+
+createStars(15, 'Small');
+createStars(6, 'Medium');
+createStars(2, 'Large');
