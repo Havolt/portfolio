134b905269606dfb8aa2cc8777c98e4eb0786b8f
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-02 15:20:01 +0000

    intro section to website with logo and text created

---
+++ scripts/portIntroScript.js
@@ -0,0 +1,4 @@
+creEl('div', ['introDiv', 'sectionMainDiv'], 'site', 0,);
+
+creEl('div', 'intLogoDiv', 'introDiv', 0, 'MF');
+creEl('div', 'intText', 'introDiv', 0 , "Hi, I'm Mark. I am a Front End Web Developer.")
