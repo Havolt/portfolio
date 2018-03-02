08cda3835e3e2a397317a9bde734d06beea66bac
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-02 14:11:31 +0000

    Started site and created functions to create elements and add click links to them

---
+++ scripts/portEditEl.js
@@ -0,0 +1,18 @@
+function creEl(el, classes, apndClass, apndClassNum, inHL, id, src){
+    let newEl = document.createElement(el);
+    if(typeof(classes) == 'string'){
+        newEl.classList.add(classes);
+    }else{
+        for(let i = 0; i < classes.length; i++){newEl.classList.add(classes[i])}
+    }
+    if(inHL){newEl.innerHTML = inHL};
+    if(id){newEl.id = id}
+    if(src){newEl.src = src}
+    document.getElementsByClassName(apndClass)[apndClassNum].appendChild(newEl);
+}
+
+function openWebPage(elClass, numOfClass, destinationLink){
+    document.getElementsByClassName(elClass)[numOfClass].addEventListener('click', function(){
+        window.open(destinationLink);
+    })
+}
