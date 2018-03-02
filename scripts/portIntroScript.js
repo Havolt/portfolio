85e473867b704c68c75d71e89a23dfc62b202b71
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-02 17:19:37 +0000

    added a movement system to stars to make page look more dynamic

--- scripts/portIntroScript.js
+++ scripts/portIntroScript.js
@@ -1,9 +1,9 @@
 creEl('div', ['introDiv', 'sectionMainDiv'], 'site', 0,);
 
-
-
 creEl('div', 'intLogoDiv', 'introDiv', 0, 'MF');
 creEl('div', 'intText', 'introDiv', 0 , "Hi, I'm Mark. I am a Front End Web Developer.")
+
+creEl('div', 'intButton', 'introDiv', 0, 'Get in touch');
 
 function createStars(amt, size){
     for(let i = 0; i < amt; i++){
@@ -15,7 +15,33 @@ function createStars(amt, size){
     }
 }
 
+function moveStars(size){
+    for(let i = 0; i < document.getElementsByClassName('intStar'+size).length; i++){
+        let nextXPos = document.getElementsByClassName('intStar'+size)[i].style.left;
+        nextXPos = nextXPos.split('%')[0];
+        if(nextXPos < 0){nextXPos = 101}
+        if(size == 'Small'){nextXPos -= 0.25;}
+        if(size == 'Medium'){nextXPos -= 0.4;}
+        if(size == 'Large'){nextXPos -= 0.7;}
+        console.log(nextXPos);
+        document.getElementsByClassName('intStar'+size)[i].style.left = nextXPos + '%';
+    }
+}
+
+function moveAll(){
+    moveStars('Small');
+    moveStars('Medium');
+    moveStars('Large');
+    setTimeout(function(){moveAll()}, 600)
+    console.log('working')
+}
+
 
 createStars(15, 'Small');
 createStars(6, 'Medium');
 createStars(2, 'Large');
+
+
+setTimeout(function(){
+moveAll();
+}, 600)
