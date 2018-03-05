916964ebabbf0fc9c2cd07bc178e6b9f530640d0
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-05 17:51:02 +0000

    added gradual scroll to about section from introMoreContain on click

--- scripts/portGenScript.js
+++ scripts/portGenScript.js
@@ -1,7 +1,15 @@
 
 
+function scrollToAbout(height){
+    let scrollRunner = window.scrollY;
+    console.log(scrollRunner)
+    if(window.scrollY < height){
+        window.scrollTo(0, scrollY + 30);
+        setTimeout(function(){scrollToAbout(height)}, 30)
+    }
+}
+
 (function initGen(){
     document.getElementsByClassName('intMoreContain')[0].addEventListener('click', function(){
-        
-    })
+        setTimeout(function(){scrollToAbout(window.innerHeight)}, 30);})
 })()
