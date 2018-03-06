2f4a20e537db4d301329c2e773aa4150b9d9d7db
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-06 15:51:01 +0000

    working on function to create elements for projectDiv, created css for projectImgOverlays

--- scripts/portProjectsScript.js
+++ scripts/portProjectsScript.js
@@ -1,8 +1,17 @@
 
+function creProjectItem(itemNum, itemImgClass, itemImgSrc){
+    creEl('div', 'projectItem', 'projectDiv', 0);
+    creEl('div', ['projectItemImg', 'projectItemImg'+itemImgClass], 'projectItem', itemNum);
+    creEl('div', ['projectItemImgOver', 'projectIIO'+itemImgClass], 'projectItem', itemNum);
+}
+
+
 (function initProjectSec(){
     creEl('div', ['projectDiv', 'sectionMainDiv'], 'site', 0);
 
     creEl('div', 'projectIntro', 'projectDiv', 0);
     creEl('div', ['projectTitle', 'sectionTitle'], 'projectIntro', 0, 'Projects');
-    creEl('div', ['projectParagraph', 'sectionParagraph'], 'projectIntro', 0, 'You can see a selection of some of my work below.')
+    creEl('div', ['projectParagraph', 'sectionParagraph'], 'projectIntro', 0, 'You can see a selection of some of my work below.');
+
+    creProjectItem(0, 'Tetris')
 })()
