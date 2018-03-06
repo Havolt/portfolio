f8984deb64846d8191ae349fab65aaef161e64b8
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-06 17:54:56 +0000

    completed first project item

--- scripts/portProjectsScript.js
+++ scripts/portProjectsScript.js
@@ -1,8 +1,11 @@
 
-function creProjectItem(itemNum, itemImgClass, itemImgSrc){
-    creEl('div', 'projectItem', 'projectDiv', 0);
+function creProjectItem(itemNum, itemImgClass,  itemImgTitle){
+    creEl('div', 'projectItemContain', 'projectDiv', 0);
+    creEl('div', 'projectItem', 'projectItemContain', itemNum);
     creEl('div', ['projectItemImg', 'projectItemImg'+itemImgClass], 'projectItem', itemNum);
     creEl('div', ['projectItemImgOver', 'projectIIO'+itemImgClass], 'projectItem', itemNum);
+    creEl('div', ['projectItemImgOverTitle', 'projectIIOT'+itemImgClass], 'projectItemImgOver', itemNum, itemImgTitle);
+    creEl('div', ['projectItemDesc', 'projectItemDesc'+itemImgClass], 'projectItemContain', itemNum, 'This is the classic video game tetris re-made entirely in vanilla JavaScript. It contains multiple features such as varying difficulty levels, score system etc');
 }
 
 
@@ -13,5 +16,5 @@ function creProjectItem(itemNum, itemImgClass, itemImgSrc){
     creEl('div', ['projectTitle', 'sectionTitle'], 'projectIntro', 0, 'Projects');
     creEl('div', ['projectParagraph', 'sectionParagraph'], 'projectIntro', 0, 'You can see a selection of some of my work below.');
 
-    creProjectItem(0, 'Tetris')
+    creProjectItem(0, 'Tetris',  '<i class="fa fa-th-large"></i> Tetris' )
 })()
