f5d2613e7f2980f6fdf4c2e2e51b62a8e6304ff0
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-06 18:23:22 +0000

    added checkers to projects section

--- scripts/portProjectsScript.js
+++ scripts/portProjectsScript.js
@@ -1,11 +1,11 @@
 
-function creProjectItem(itemNum, itemImgClass,  itemImgTitle){
+function creProjectItem(itemNum, itemImgClass,  itemImgTitle, itemImgDesc){
     creEl('div', 'projectItemContain', 'projectDiv', 0);
     creEl('div', 'projectItem', 'projectItemContain', itemNum);
     creEl('div', ['projectItemImg', 'projectItemImg'+itemImgClass], 'projectItem', itemNum);
     creEl('div', ['projectItemImgOver', 'projectIIO'+itemImgClass], 'projectItem', itemNum);
-    creEl('div', ['projectItemImgOverTitle', 'projectIIOT'+itemImgClass], 'projectItemImgOver', itemNum, itemImgTitle);
-    creEl('div', ['projectItemDesc', 'projectItemDesc'+itemImgClass], 'projectItemContain', itemNum, 'This is the classic video game tetris re-made entirely in vanilla JavaScript. It contains multiple features such as varying difficulty levels, score system etc');
+    creEl('div', ['projectItemImgOverTitle', 'projectIIOT'+itemImgClass], 'projectItemImgOver', itemNum, itemImgTitle + ' ' + itemImgClass);
+    creEl('div', ['projectItemDesc', 'projectItemDesc'+itemImgClass], 'projectItemContain', itemNum, itemImgDesc );
 }
 
 
@@ -16,5 +16,6 @@ function creProjectItem(itemNum, itemImgClass,  itemImgTitle){
     creEl('div', ['projectTitle', 'sectionTitle'], 'projectIntro', 0, 'Projects');
     creEl('div', ['projectParagraph', 'sectionParagraph'], 'projectIntro', 0, 'You can see a selection of some of my work below.');
 
-    creProjectItem(0, 'Tetris',  '<i class="fa fa-th-large"></i> Tetris' )
+    creProjectItem(0, 'Tetris',  '<i class="fa fa-th-large"></i>', 'This is the classic video game tetris re-made entirely in vanilla JavaScript. It contains multiple features such as varying difficulty levels, score system etc.')
+    creProjectItem(1, 'Checkers', '<i class="fa fa-delicious"></i>', 'My own version of Checkers created using vanilla JavaScript. It contains most of the offical rules of checkers including mandatory takes and multi-takes.' )
 })()
