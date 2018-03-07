77fe4d0651713089bd78ddb28fe24e7f21f99915
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-07 19:37:02 +0000

    twitter and github links added to footer and styling changes done to general footer section

--- scripts/portFooterScript.js
+++ scripts/portFooterScript.js
@@ -1,7 +1,7 @@
 
-function createFooterLink(linkName, linkNum, inHL, linkDestination){
+function createFooterLink(linkName,  inHL, linkDestination){
     creEl('div', ['footerLink', 'footerLink'+ linkName], 'footerLinksDiv', 0);
-    creEl('div', ['footerLinkIcon', 'footerLinkIcon'+linkName], 'footerLink', linkNum, '<i class="fa fa-'+inHL+'"></i>');
+    creEl('div', ['footerLinkIcon', 'footerLinkIcon'+linkName], 'footerLink'+ linkName, 0, '<i class="fa fa-'+inHL+'"></i>');
     openWebPage('footerLink'+linkName, 0, linkDestination)
 }
 
@@ -9,7 +9,9 @@ function createFooterLink(linkName, linkNum, inHL, linkDestination){
     creEl('div', ['footerDiv', 'sectionMainDiv'], 'site', 0);
 
     creEl('div', 'footerLinksDiv', 'footerDiv', 0);
-    createFooterLink('Linkedin', 0, 'linkedin', 'https://www.linkedin.com/in/mark-fitzpatrick-491419100/');
+    createFooterLink('Linkedin', 'linkedin', 'https://www.linkedin.com/in/mark-fitzpatrick-491419100/');
+    createFooterLink('Twitter', 'twitter', 'https://twitter.com/jetsetfitz');
+    createFooterLink('Github', 'github', 'https://github.com/Havolt');
 
     creEl('div', 'copyrightDiv', 'footerDiv', 0, 'Created by me &copy; 2018');
 })()
