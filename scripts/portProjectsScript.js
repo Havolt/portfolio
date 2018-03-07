89cc26f1b5b3f8c09a40bd7a60dcd4b03c7bdd38
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-07 14:50:50 +0000

    added more button to project section and created contact style and script page

--- scripts/portProjectsScript.js
+++ scripts/portProjectsScript.js
@@ -19,4 +19,8 @@ function creProjectItem(itemNum, itemImgClass,  itemImgTitle, itemImgDesc){
     creProjectItem(0, 'Tetris',  '<i class="fa fa-th-large"></i>', 'This is the classic video game tetris re-made entirely in vanilla JavaScript. It contains multiple features such as varying difficulty levels, score system etc.');
     creProjectItem(1, 'Checkers', '<i class="fa fa-delicious"></i>', 'My own version of Checkers created using vanilla JavaScript. It contains most of the offical rules of checkers including mandatory takes and multi-takes.' );
     creProjectItem(2, 'To-Do', '<i class="fa fa-calendar"></i>', 'A To-Do list written in pure JavaScript. This app allows the user to create items, favourite them, delete them and allows the user to cycle between different display options.')
+
+    creEl('div', 'projectMore', 'projectDiv', 0);
+    creEl('div', 'projectMoreText', 'projectMore', 0, 'If you would like to view more of my work please use the following link:');
+    creEl('div', 'projectMoreButton', 'projectMore', 0, 'View More');
 })()
