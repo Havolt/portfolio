f0ff68294821e534996e84baf85bf029ed7772d1
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-12 18:54:25 +0000

    added styling to header in under 620px and above sections

--- scripts/portProjectHeader.js
+++ scripts/portProjectHeader.js
@@ -1,12 +1,19 @@
 
-function linkMaker(){
-    creEl('div', '')
+function linkMaker(linkName, lastLinkBool){
+    creEl('div', ['headerLink', 'headerLink'+linkName], 'headerLinksContain', 0, linkName  )
 }
 
 (function initProjectHeader(){
     creEl('div', 'projectHeader', 'main', 0);
     creEl('div', 'getBack', 'projectHeader', 0, '<i class="fa fa-angle-double-left"></i>');
-    creEl('div', 'headerLinks', 'projectHeader', 0);
+    creEl('div', 'headerLinksContain', 'projectHeader', 0);
+
+    linkMaker('Tetris')
+    linkMaker('Checkers');
+    linkMaker('To-do');
+    linkMaker('Calculator');
+    linkMaker('wiki-API');
+    linkMaker('Snake');
 
     openWebPage('getBack', 0, 'index.html', true)
 })()
