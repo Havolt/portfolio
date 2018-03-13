085a887264cdc73f0b9b0fe5be0d2dfaca13b62c
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-13 19:29:43 +0000

    disabled arrow keys outside of controlling tetris and added styling for smaller size screens

--- projects/scripts/portProjectTetris.js
+++ projects/scripts/portProjectTetris.js
@@ -841,6 +841,12 @@ function createShapeDataCaller(obj){
 
                                         ///// Adds event listener which allows user to control game /////
 
+window.addEventListener("keydown", function(e) {
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 document.addEventListener('keydown', function(e){
     playerControl(e.keyCode)
 })
