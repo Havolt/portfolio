888d47e091c96974ebb4584c7bf14608dcfcfe85
Mark Fitzpatrick <markfitz815@gmail.com>
2018-03-15 15:38:11 +0000

    added styling to about section and further text added to about text intro, some hidden in mobile size

--- scripts/portAboutScript.js
+++ scripts/portAboutScript.js
@@ -11,15 +11,17 @@ function createAboutListItem(listItemName, iconHL, titleHL, paraHL){
 (function initAboutSec(){
     creEl('div', ['aboutDiv', 'sectionMainDiv'], 'main', 0);
 
-    creEl('div', 'aboutIntro', 'aboutDiv', 0)
-    creEl('div', ['aboutTitle', 'sectionTitle'], 'aboutIntro', 0, 'About Me');
-    creEl('div', ['aboutParagraph', 'sectionParagraph'], 'aboutIntro', 0, "I have a large range of Front End skills ranging from design to HTML, CSS, JavaScript and GitHub and multiple frameworks. ")
+    creEl('div', 'aboutIntro', 'aboutDiv', 0);
+    creEl('div', 'aboutIntroText', 'aboutIntro', 0);
+    creEl('div', ['aboutTitle', 'sectionTitle'], 'aboutIntroText', 0, 'About Me');
+    creEl('div', ['aboutParagraph', 'sectionParagraph'], 'aboutIntroText', 0, "I'm an experienced Front End web developer. I have spent years perfecting my skills in JavaScript and CSS. I did a degree in multimedia which dealt with topics such as web design, programming and graphic design. Since then I have spent my time creating numerous complex projects in JavaScript. ")
+    creEl('div', ['aboutParagraph', 'sectionParagraph', 'aboutParagraphHidden'], 'aboutIntroText', 0, 'I am currently looking for a challenging front end position which will utilize my talents. I have experience with gitHub and many JavaScript frameworks.')
     creEl('div', 'aboutProfilePic', 'aboutIntro', 0);
     creEl('img', 'aboutPPImg', 'aboutProfilePic', 0, '', '', 'images/pp1.jpg');
 
     creEl('div', 'aboutListContain', 'aboutDiv', 0);
     createAboutListItem('Responsive', '<i class="fa fa-mobile"></i>', 'Responsive Design', 'I put a strong focus on creating responsive websites to fit all screen sizes and types. I build from the ground up to fit mobile first design practices and make beautiful websites from the ground up.');
-    createAboutListItem('Code', '<i class="fa fa-code"></i>', 'Programming Skills', 'I have very strong JavaScropt skills which I utilize to create interactive pages . In the past I have created complicated applications such as checkers and tetris with vanilla JavaScript.'  )
+    createAboutListItem('Code', '<i class="fa fa-code"></i>', 'Programming Skills', 'I have very strong JavaScript skills which I utilize to create interactive pages . In the past I have created complicated applications such as checkers and tetris with vanilla JavaScript.'  )
     createAboutListItem('Design', '<i class="fa fa-paint-brush"></i>', 'Beautiful Interfaces', 'I have gained a lot of knowledge on creating elegant, user friendly websites where inormation is presented in a clear and eye-catching way.');
 
 })()
